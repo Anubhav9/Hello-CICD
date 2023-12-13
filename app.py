12a3c7eb60b48b7261db3ed7c46b42ed3c4f6b56
Anubhav Sanyal <princebest3@rediffmail.com>
2023-12-13 22:03:36 +0530

    added exposed host

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def open_home_page():
     return render_template("index.html",itr_count=constants.ITERATION_COUNT)
 
 if(__name__=="__main__"):
-    app.run(port=1212,debug=True)
+    app.run(port=1212,debug=True,host='0.0.0.0')
